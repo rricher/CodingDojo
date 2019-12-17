e442e7c1099d15928bc547d797f7ab2f72378ac2
rricher <ryanricher@comcast.net>
2019-12-16 18:49:48 -0800

    night 6 1.1

---
+++ JS/playingWithObjects.js
@@ -0,0 +1,6 @@
+var users = [{name: "Michael", age:37}, {name: "John", age:30}, {name: "David", age:27}];
+console.log(users[1].age);
+console.log(users[0].name);
+for (var i in users) {
+    console.log(users[i].name, ' - ', users[i].age);
+}
